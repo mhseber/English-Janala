7c498e7763ddb253c59e2a0532273ddccc369aeb
mahamudul hasan seber <sebermh@gmail.com>
2026-02-11 16:31:46 +0600

    Design & Display Dynamic Cards

--- script/index.js
+++ script/index.js
@@ -13,13 +13,24 @@ const loadLevelWord = (id)=>{
 
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
-    // wordContainer.innerHTML = "";
+    wordContainer.innerHTML = "";
+
+
+
 
     words.forEach((word) => {
         console.log(word);
         const card = document.createElement("div");
         card.innerHTML = `
-        <p>Cat</p>
+          <div class="px-4 py-10 space-y-4 text-center bg-white shadow-md rounded-xl">
+    <h2 class="text-3xl font-bold text-gray-800">${word.word}</h2>
+    <p class="font-semibold">Meaning /Pronounciation</p>
+    <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+    <div class="flex items-center justify-between">
+      <button class="btn bg-[#1491ff10] hover:bg-[#1491ff80] "><i class="fa-solid fa-circle-info"></i></button>
+      <button class="btn bg-[#1491ff10] hover:bg-[#1491ff80]"><i class="fa-solid fa-volume-low"></i></button>
+    </div>
+  </div>
         `;
         wordContainer.append(card);
 
